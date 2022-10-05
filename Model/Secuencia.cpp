dc52136f4fa22e4e35680d98b38300e97643389a
Sebastian Guarnizo Campos <62730462+sebastian0912@users.noreply.github.com>
2022-10-05 12:08:56 -0500

    Parte de maikol hecha

--- Model/Secuencia.cpp
+++ Model/Secuencia.cpp
@@ -29,7 +29,8 @@ void Secuencia::guardar(list<Secuencia> &lista, string nombre_archivo)
             fe.getline(str, MAX_LENGHT);
             if (str[0] == '>')
             {
-                
+                if (sec != "")
+                {
                     Secuencia secuencia;
                     secuencia.setNombre_sec(datos);
                     secuencia.setSecuencia(sec);
@@ -39,8 +40,8 @@ void Secuencia::guardar(list<Secuencia> &lista, string nombre_archivo)
                     lista.push_back(secuencia);
                     sec = "";
                     datos = "";
-                    aux = 0;
-                
+                    cont = 0;
+                }
                 datos = str;
             }
             else
@@ -61,7 +62,7 @@ void Secuencia::guardar(list<Secuencia> &lista, string nombre_archivo)
         Secuencia secuencia;
         secuencia.setNombre_sec(datos);
         secuencia.setSecuencia(sec);
-        secuencia.setTamano(cont);
+
         lista.push_back(secuencia);
         sec = "";
         aux=0;
@@ -70,8 +71,6 @@ void Secuencia::guardar(list<Secuencia> &lista, string nombre_archivo)
     {
         cout << "No se pudo abrir el archivo" << endl;
     }
-    fe.close();
-    lista.pop_front();
     cout << "Secuencias guardadas" << endl;
     for (list<Secuencia>::iterator it = lista.begin(); it != lista.end(); ++it)
     {
@@ -83,3 +82,78 @@ void Secuencia::guardar(list<Secuencia> &lista, string nombre_archivo)
     fe.close();
 }
 
+void listarSecuenciaw(vector<char> cod, int &aux)
+{
+    int cont = 0, cont2 = 0;
+    vector<char> codigos = {'A', 'C', 'G', 'T', 'U', 'R', 'Y', 'K', 'M', 'S', 'W', 'B', 'D', 'H', 'V', 'N', 'X', '-'};
+    for (int i = 0; i < codigos.size(); i++)
+    {
+        vector<char>::iterator it = cod.begin();
+        while (it != cod.end())
+        {
+            if (*it == codigos[i])
+            {
+                cont++;
+            }
+            if (*it == '-')
+            {
+                cont2++;
+            }
+            it++;
+        }
+
+        if (cont != 0 && cont2 != 0 && codigos[i] != '-')
+        {
+            aux -= cont;
+            cout << codigos[i] << " Contiene: " << cont << " bases" << endl;
+        }
+        else if (cont != 0 && cont2 == 0 && codigos[i] != '-')
+        {
+            aux += cont;
+            cout << codigos[i] << " Contiene: " << cont << " bases" << endl;
+        }
+        cont = 0;
+        cont2 = 0;
+    }
+}
+
+void listarSecuencias(list<Secuencia> lista)
+{    
+    int cont = 0;
+    // Cuantas veces cada codigo aparece en la secuencia por cada codigo genetico
+    for (list<Secuencia>::iterator it = lista.begin(); it != lista.end(); ++it)
+    {
+        vector<char> secuencias;
+        for (int i = 0; i < it->getSecuencia().size(); i++)
+        {
+            secuencias.push_back(it->getSecuencia()[i]);
+        }
+        vector<char>::iterator it2 = secuencias.begin();
+
+        // it2 vacio
+        if (it2 == secuencias.end())
+        {
+            break;
+        }
+        else
+        {
+            cout << it->getNombre_sec() << endl;
+            listarSecuenciaw(secuencias, cont);
+            if (cont < 1)
+            {
+                cout << "Esta secuencia contiene al menos " << cont * -1 << " bases" << endl;
+            }
+            else
+            {
+                cout << "Esta secuencia contiene " << cont << " bases" << endl;
+            }
+            cont = 0;
+        }
+        for (int i = 0; i < secuencias.size(); i++)
+        {
+            cout << secuencias[i];
+        }
+        secuencias.clear();
+        system("pause");
+    }
+}
